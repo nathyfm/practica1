70661bbf4d84f60133ebdef7a0cbf763e16c3ad7
Natalia Florez <natalia.florezm1@udea.edu.co>
2023-10-25 15:50:48 -0500

    Se hizo login para admin

--- src/components/IngresarView.vue
+++ src/components/IngresarView.vue
@@ -1,31 +1,23 @@
 <template>
     <div class="container">
-        
         <div class="card">
             <div class="card-header">
                 <b>Ingreso: Especialista de Laboratorio</b>
             </div>
             <div class="card-body">
-                <form v-on:submit.prevent="agregarRegistro">
+                <form v-on:submit.prevent="iniciarSesion">
                     <div class="form-group">
-                      <label for="doc">Número de identificación</label>
-                      <input type="number"
-                        class="form-control" name="doc" v-model="paciente.doc" id="doc" aria-describedby="helpId" placeholder="">
-                      <small id="helpId" class="form-text text-muted">Ingrese su número de identificación</small>
+                        <label for="user">Usuario</label>
+                        <input type="text" class="form-control" name="user" v-model="admin.user" id="user" placeholder="">
                     </div>
- 
                     <div class="form-group">
-                      <label for="password">Contraseña</label>
-                      <input type="password"
-                        class="form-control" name="lastname"  v-model="paciente.lastname" id="lastname" aria-describedby="helpId" placeholder="">
-                      <small id="helpId" class="form-text text-muted">Ingrese su contraseña</small>
+                        <label for="password">Contraseña</label>
+                        <input type="password" class="form-control" name="password" v-model="admin.password" id="password" placeholder="">
                     </div>
                     <div class="btn-group" role="group" aria-label="">
                         <button type="submit" class="btn btn-success">Ingresar</button>
                         <button type="reset" class="btn btn-warning">Limpiar</button>
                     </div>
-
-
                 </form>
             </div>
             <div class="card-footer text-muted">
@@ -35,26 +27,36 @@
     </div>
 </template>
 
+
 <script>
 export default {
-    data(){
-        return{
-            paciente:{}
-        }
+    data() {
+        return {
+            admin: {
+                user: "",
+                password: ""
+            }
+        };
     },
-    methods:{
-        agregarRegistro(){
-            console.log(this.paciente)
-            let datosEnviar={id:null , doc:this.paciente.doc , name:this.paciente.name , lastname:this.paciente.lastname}
-            fetch('http://localhost/api/?insertar=1',{
-                method:"POST",
-                body:JSON.stringify(datosEnviar)
+    methods: {
+        iniciarSesion() {
+            console.log(this.admin);
+            let datosAdmin={id:null , user:this.admin.user , password:this.admin.password}
+            // Enviar las credenciales al servidor para validar el inicio de sesión
+            fetch('http://localhost/api/?iniciar_sesion=1', {
+                method: "POST",
+                body: JSON.stringify(datosAdmin)
             })
-            .then(respuesta=>respuesta.json())
-            .then((datosRespuesta=>{
-                console.log(datosRespuesta);
-                window.location.href='consultar' //Redirecciona a la ruta consultar
-            }))
+            .then(respuesta => respuesta.json())
+            .then(datosAdminRespuesta => {
+                if (datosAdminRespuesta.success === 1) {
+                    // Las credenciales son válidas, redirigir al usuario
+                    window.location.href = 'consultar'; // Redirecciona a la ruta para ver todos los pacientes
+                } else {
+                    // Las credenciales son inválidas, mostrar mensaje de error
+                    alert("Credenciales inválidas. Inténtalo de nuevo.");
+                }
+            });
         }
     }
 }
